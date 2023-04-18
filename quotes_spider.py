b1384a1bb80f502f20644073eb3b231daee324b4
Ingabire Yvette <yvette.ingabire@live.fr>
2023-04-18 05:58:44 +0200

    spider

--- quotes_spider.py
+++ quotes_spider.py
@@ -10,3 +10,4 @@ class QuotesSpiderSpider(scrapy.Spider):
         title = response.css('title:text').extract()
         yield {'title text' : title }
        
+#dans le terminal : scrapy shell "https://www.imdb.com/chart/top/?ref_=nv_mv_250" pour scraper les donner des titres des films du site imdb
